a02d48b12eb821e1a4a5331913a56b4b7b012a2d
Aaron Griffith <aargri@gmail.com>
2015-03-06 20:16:26 -0500

    note to self: python does not have a symbol type

--- scene.py
+++ scene.py
@@ -299,7 +299,7 @@ class TungstenScene:
             num_triangles += 1
 
         uvdata = None
-        if getattr(m, tessface_uv_textures, None) and len(m.tessface_uv_textures) >= 1:
+        if getattr(m, 'tessface_uv_textures', None) and len(m.tessface_uv_textures) >= 1:
             uvdata = m.tessface_uv_textures[0].data # FIXME many uv maps
         
         for n, f in enumerate(m.tessfaces):
