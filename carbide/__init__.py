0e5769d708bacee1665e37b05cfcf84eed9687b5
Aaron Griffith <aargri@gmail.com>
2021-05-27 18:11:34 -0400

    add some code to __init__.py to unload submodules on reload

--- carbide/__init__.py
+++ carbide/__init__.py
@@ -1,3 +1,12 @@
+# some reload machinery -- make sure to unload submodules
+# so that importlib.reload(carbide) reloads *everything*
+# this is entirely so that we can reload the plugin in blender during dev
+
+import sys
+
+for m in [m for m in sys.modules.keys() if m.startswith('carbide.')]:
+    del sys.modules[m]
+
 from carbide.blender import register, unregister
 from carbide.tungsten import *
 
